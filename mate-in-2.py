f989fda6ccde71af2d917c111761a85c794063f6
Davanum Srinivas <davanum@gmail.com>
2024-11-30 08:50:35 -0500

    Switch to another nitter instance
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- mate-in-2.py
+++ mate-in-2.py
@@ -25,23 +25,10 @@ def get_profile_feed(client, bsky_id):
 
 def get_mate_in_2_posts():
     response = feedparser.parse(
-        'https://nitter.poast.org/ImShahinyan/rss',
+        'https://nitter.privacydev.net/ImShahinyan/rss',
         request_headers={
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9,it;q=0.8',
-            'cache-control': 'max-age=0',
-            'cookie': 'res=69C35A40B4CE1ABFDDA1764EA815CCEB7F305C08129443',
-            'dnt': '1',
-            'priority': 'u=0, i',
-            'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'sec-gpc': '1',
-            'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
         },
     )
